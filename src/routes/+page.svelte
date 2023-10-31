afb89d9a93b10074f7fba12ab830875db00524c6
Tushar <tusharmane.tm@gmail.com>
2023-10-31 11:07:24 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="grid gap-6">
+	<header class="border p-4 gap-6 flex justify-between items-center">
+		<div class="font-bold uppercase text-blue text-24">Logitech</div>
+		<div class=""><iconify-icon class="text-30" icon="mdi:cart-outline"></iconify-icon></div>
+	</header>
+
+	<main class="grid grid-flow-col gap-6 md:[grid-template-columns:250px_auto]">
+		<aside class="border p-4">
+			Filters
+		</aside>
+		<section  class="border p-4">
+			<h2 class="font-bold pb-4 text-20">Main Content</h2>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi rerum incidunt tempore voluptates! Delectus qui similique at est ratione, necessitatibus blanditiis quasi animi, eaque, totam nobis alias quam nam maiores?</p>
+		</section>
+	</main>
+
+	<footer class="p-4 flex justify-between items-center gap-6 bg-black text-white">
+		<p class="italic">&#169;2023 Logitech</p>
+		<nav class="flex p-2 gap-4 text-blue">
+			<a href="/about">About</a>
+			<a href="/contact">Contact</a>
+			<a href="/support">Support</a>
+			<a href="/privacy">Privacy</a>
+		</nav>
+	</footer>
+</div>
